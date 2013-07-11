251ae650e8b0c4d8211e21dde8f4cd6607792203
Andrew Mirsky <andrew@mirsky.net>
2013-07-11 14:14:06 -0400

    enabling the manifest file

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='MirskUtils',
       version='0.1',
@@ -8,7 +9,8 @@ setup(name='MirskUtils',
       author='Andrew',
       author_email='andrew@mirsky.net',
       url='http://andrew.mirsky.com/',
-      packages=['mirskutils', 'mirskutils.templatetags',],
+      packages=find_packages(),
+      include_package_data=True
      )
 
 
