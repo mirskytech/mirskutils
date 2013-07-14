b121bc74c79d722151d6092cb11f1800d4215df8
Andrew Mirsky <andrew@mirsky.net>
2013-07-14 08:58:20 -0400

    updated dependency list

--- setup.py
+++ setup.py
@@ -3,6 +3,33 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+dependencies = [
+    'Django',
+    'PIL',
+    'jsonfield',
+    'django-retracer',
+    'South',
+    'Sphinx',
+    'boto',
+    'django-admin-tools',
+    'django-bitfield',
+    'celery'
+    'django-celery',
+    'django-compressor',
+    'django-fsm',
+    'django-retracer',
+    'django-sekizai',
+    'django-social-auth',
+    'jsonfield',
+    'psycopg2',
+    'requests',
+    'sphinxcontrib-fancybox',
+]
+
+
+
+
+
 setup(name='MirskUtils',
       version='0.1',
       description='Mirsky Utility Functions',
@@ -11,7 +38,7 @@ setup(name='MirskUtils',
       url='http://andrew.mirsky.com/',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['Django','PIL']
+      install_requires=dependencies
      )
 
 
