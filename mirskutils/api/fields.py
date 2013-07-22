01b7f5d052672e56759a5e8f3a9d5a5b1aa64c93
Andrew Mirsky <andrew@mirsky.net>
2013-07-22 11:35:19 -0400

    added field which checks authorization limits fo related resources before returning them

--- mirskutils/api/fields.py
+++ mirskutils/api/fields.py
@@ -1,4 +1,7 @@
 from tastypie.fields import ToManyField, NOT_PROVIDED
+from tastypie.bundle import Bundle
+from tastypie.exceptions import ApiFieldError, NotFound
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 
 
@@ -14,3 +17,54 @@ class ToManySpecifyField(ToManyField):
         
     def dehydrate_related(self, bundle, related_resource, for_list=True):
         return { field:getattr(bundle.obj, field) for field in self.fields }
+    
+    
+class ToManyAuthorizedField(ToManyField):
+    
+    def dehydrate(self, bundle, for_list=True):
+        if not bundle.obj or not bundle.obj.pk:
+            if not self.null:
+                raise ApiFieldError("The model '%r' does not have a primary key and can not be used in a ToMany context." % bundle.obj)
+
+            return []
+
+        the_m2ms = None
+        previous_obj = bundle.obj
+        attr = self.attribute
+
+        if isinstance(self.attribute, basestring):
+            attrs = self.attribute.split('__')
+            the_m2ms = bundle.obj
+
+            for attr in attrs:
+                previous_obj = the_m2ms
+                try:
+                    the_m2ms = getattr(the_m2ms, attr, None)
+                except ObjectDoesNotExist:
+                    the_m2ms = None
+
+                if not the_m2ms:
+                    break
+
+        elif callable(self.attribute):
+            the_m2ms = self.attribute(bundle)
+
+        if not the_m2ms:
+            if not self.null:
+                raise ApiFieldError("The model '%r' has an empty attribute '%s' and doesn't allow a null value." % (previous_obj, attr))
+
+            return []
+
+        self.m2m_resources = []
+        m2m_dehydrated = []
+
+        # TODO: Also model-specific and leaky. Relies on there being a
+        #       ``Manager`` there.
+        for m2m in the_m2ms.all():
+            m2m_resource = self.get_related_resource(m2m)
+            m2m_bundle = Bundle(obj=m2m, request=bundle.request)
+            if m2m_resource._meta.authorization.read_detail(m2m, m2m_bundle):
+                self.m2m_resources.append(m2m_resource)
+                m2m_dehydrated.append(self.dehydrate_related(m2m_bundle, m2m_resource, for_list=for_list))
+
+        return m2m_dehydrated    
