3e4d0ef7f35dc941e9ffd9fcad9801b59fd6a847
Andrew Mirsky <andrew@mirsky.net>
2013-07-31 17:57:26 -0400

    updating login required view to also check permissions

---
+++ mirskutils/nosql.py
@@ -0,0 +1,77 @@
+
+
+from couchdb import client, PreconditionFailed
+from django.conf import settings
+from couchdb.design import ViewDefinition
+from uuid import uuid4
+
+
+class Server(object):
+        
+    def __init__(self, name, model_class, *args, **kwargs):
+        self._db = None
+        self.name = name
+        self._registry = [] # simple at first, just register the ViewDefinition
+        self.klass = model_class
+    
+    # since this could be called from a fork, we're not guaranteed a connection
+    # http://code.google.com/p/couchdb-python/issues/detail?id=205    
+    def db(self):
+        if not self._db:
+            server = client.Server(url=settings.COUCHDB_SERVER)    
+            server.resource.credentials = (settings.COUCHDB_USERNAME, settings.COUCHDB_PASSWORD)
+            try:
+                server.create(self.name)
+            except PreconditionFailed:
+                pass
+            finally:
+                self._db = server[self.name]
+                
+        return self._db
+    
+    def load(self, _id):
+        return self.klass.load(self.db(), _id)
+    
+    def store(self, obj):
+        if isinstance(obj, self.klass):
+            return obj.store(self.db())
+        raise TypeError("'%s' should be of type %s" % (type(obj),self.klass.__name__))
+
+    def remove(self, obj):
+        if isinstance(obj, self.klass):
+            return self.db().delete(obj)
+        raise TypeError("'%s' should be of type %s" % (type(obj),self.klass.__name__))
+    
+    def sync(self, view_definition, force=False):
+        if not force:
+            # TODO ?? check to see if the view is already registered. by pass if it is. (?)
+            raise PreconditionFailed('View syncing causes index rebuilds. Force sync required.')
+        return view_definition.sync(self.db())
+    
+    def sync_all(self):
+        # TODO : add ability to pass in a list of ViewDefinitions (?)
+        # TODO : optimization check the db to see if the viewdefinition has changed (compare function?) (?)
+        for vd in self._registry:
+            self.sync(vd, True)
+        
+    def register(self, view_definition):
+        if not isinstance(view_definition, ViewDefinition):
+            raise TypeError('%s should be of type \'couchdb.design.ViewDefinition\'')
+        
+        # TODO: should check that two views aren't registered with the same primary/secondary key
+        self._registry.append(view_definition)
+    
+    def create(self, main, secondary, function):
+        view_definition = ViewDefinition(main, secondary, function)
+        return self.sync(view_definition)
+    
+    def view(self, view, **params):
+        return self.klass.view(self.db(), view, **params)
+    
+    def copy(self, doc_or_id):
+        _id = uuid4().hex
+        self.db().copy(doc_or_id, _id)
+        return self.load(_id)
+    
+    def query(self, view_function):
+        return self.db().query(view_function)
--- mirskutils/views.py
+++ mirskutils/views.py
@@ -1,11 +1,29 @@
 from django.views.generic import View
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required, permission_required
+from django.conf import settings
 from django.core.urlresolvers import reverse_lazy
-from utils.decorators import method_decorator
+
+from django.core.exceptions import PermissionDenied
+
+
+from decorators import method_decorator
 
 
 class LoginRequiredView(View):
+
+    permissions = []
+    redirect_401 = settings.LOGIN_URL
+    redirect_403 = reverse_lazy('forbidden403')
         
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(LoginRequiredView, self).dispatch(*args, **kwargs)
+    #@method_decorator(login_required)
+    #@method_decorator(permission_required(['people.verifier_express',]))
+    def dispatch(self, request, *args, **kwargs):
+        
+        if not request.user.is_authenticated():
+            return redirect(self.redirect_401)
+    
+        if self.permissions and not request.user.has_perm(*self.permissions):
+            return redirect(self.redirect_403)
+        
+        return super(LoginRequiredView, self).dispatch(request, *args, **kwargs)
