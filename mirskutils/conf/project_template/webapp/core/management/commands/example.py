72c2b92e5a9c70706f4dd3cd4e28f7000dbd4116
Andrew Mirsky <andrew@mirsky.net>
2014-01-10 20:43:02 -0500

    updating example management commands and added argument exception

--- mirskutils/conf/project_template/webapp/core/management/commands/example.py
+++ mirskutils/conf/project_template/webapp/core/management/commands/example.py
@@ -1,16 +1,34 @@
 
-from django.core.management.base import BaseCommand
+# uses optparser. although deprecated, django's commands are based on it
 
-
-
-class Command(BaseCommand):
+#from django.core.management.base import BaseCommand
+#class Command(BaseCommand):
 
     #astring = make_option('-a', '--aaaa', action="store", type="string", dest="aaa_variable")
     #bboolean = make_option('-b', '--bbbb', action="store_true", type="string", dest="bbb_variable")
 
     #option_list = BaseCommand.option_list + (astring,bbooleans)
 
-    def handle(self, *arguments, **options):
-        pass
+    #def handle(self, *arguments, **options):
+        #pass
     
-    
+
+
+# uses newer pythong argparser (does not support default options like traceback and no-input, yet)
+
+#from mirskutils.management.base import ArgBaseCommand
+#class Command(ArgBaseCommand):
+        
+        #def add_arguments(self, argparser):
+                #group = argparser.add_mutually_exclusive_group(required=True)   
+                #group.add_argument('-a','--aa')
+                #group.add_argument('-b','--bb')
+                #argparser.add_argument('position1')
+                #argparser.add_argument('position2') 
+                        
+        #def handle(self, arguments):
+            
+            #mutually_exclusive = getattr(arguments,'aa', getattr(arguments,'bb'))
+            #position_arg_1 = arguments.position1
+            
+            
