4f985b8ae2dc9125baf9886dfd50c9c63af8e65b
Andrew Mirsky <andrew@mirsky.net>
2014-01-24 07:32:09 -0500

    adding version string to package, according to http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
 
+import re
+
 from distutils.core import setup
 from setuptools import find_packages
+
+
+VERSIONFILE="mirskutils/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 
 dependencies = [
     'Django',
@@ -52,7 +65,7 @@ links = [
 ]
 
 setup(name='MirskUtils',
-      version='0.1',
+      version=verstr,
       description='Mirsky Utility Functions',
       author='Andrew',
       author_email='andrew@mirsky.net',
