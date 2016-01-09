9ffcfc9e4806dc823037288b6a033e639f78a1f2
Andrew Mirsky <andrew@mirsky.net>
2016-01-09 16:54:46 -0500

    replacing withas template tag with a with as decorator for template tags

--- mirskutils/template.py
+++ mirskutils/template.py
@@ -2,52 +2,53 @@ from django.template import Library as DjangoTemplateLibrary
 from inspect import getargspec
 from django.template.library import TagHelperNode, parse_bits
 
-class Library(DjangoTemplateLibrary):
+from functools import wraps
 
-    #https://djangosnippets.org/snippets/1701/
+# from https://djangosnippets.org/snippets/1701/
+
+def with_as(f):
+    """
+    Decorator enabling a simple template tag to support "as my_var"
+    syntax. When an as varible specified the result is added to the
+    context under the variable name.
+
+    example:
+        @with_as
+        @register.simple_tag
+        def do_current_time(parser, token):
+            ...
+            return a_node
+
+        {% current_time %}
+
+        {% current_time as time %}
+        {{ time }}
+    """
+    @wraps(f)
+    def new_f(parser, token):
+        contents = token.split_contents()
+
+        if len(contents) < 3 or contents[-2] != 'as':
+            return f(parser, token)
+
+        as_var = contents[-1]
+        # Remove 'as var_name' part from token
+        token.contents =  ' '.join(contents[:-2])
+        node = f(parser, token)
+        patch_node(node, as_var)
+        return node
+    return new_f
+
+def patch_node(node, as_var):
+    """
+    Patch the render method of node to silently update the context.
+    """
+    node._old_render = node.render
+
+    # We patch a bound method, so self is not required
+    @wraps(node._old_render)
+    def new_render(context):
+        context[as_var] = node._old_render(context)
+        return ''
     
-    
-    
-    def withas_tag(self, func=None, takes_context=None, name=None):
-        def dec(func):
-            params, varargs, varkw, defaults = getargspec(func)
-    
-            class WithAsNode(TagHelperNode):
-                def __init__(self, takes_context, args, kwargs, target_var=None):
-                    super(WithAsNode, self).__init__(takes_context, args, kwargs)
-                    self.target_var = target_var
-    
-                def render(self, context):
-                    resolved_args, resolved_kwargs = self.get_resolved_arguments(context)
-                    if self.target_var:
-                        context[self.target_var] = func(*resolved_args, **resolved_kwargs)
-                        return ''
-                    return func(*resolved_args, **resolved_kwargs)
-    
-            function_name = (name or
-                getattr(func, '_decorated_function', func).__name__)
-    
-            def compile_func(parser, token):
-                bits = token.split_contents()[1:]
-                
-                target_var = None
-                if len(bits) >=2 and bits[-2] == 'as':
-                    target_var = bits[-1]
-                    bits = bits[:-2]
-                    
-                args, kwargs = parse_bits(parser, bits, params,
-                    varargs, varkw, defaults, takes_context, function_name)
-                return WithAsNode(takes_context, args, kwargs, target_var)
-    
-            compile_func.__doc__ = func.__doc__
-            self.tag(function_name, compile_func)
-            return func
-    
-        if func is None:
-            # @register.assignment_tag(...)
-            return dec
-        elif callable(func):
-            # @register.assignment_tag
-            return dec(func)
-        else:
-            raise TemplateSyntaxError("Invalid arguments provided to assignment_tag")
+    node.render = new_render
